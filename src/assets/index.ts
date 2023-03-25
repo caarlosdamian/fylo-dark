673d1afd8d639051c85d13dc296535579c889d88
caarlosdamian <dapecarlos@gmail.com>
2023-03-25 14:31:31 -0600

    feat(assests): added new assest

---
+++ src/assets/index.ts
@@ -0,0 +1,36 @@
+import bgCurvyDesktop from "./bg-curvy-desktop.svg";
+import bgCurvyMobile from "./bg-curvy-mobile.svg";
+import bgQuotes from "./bg-quotes.png";
+import iconAccessAnywhere from "./icon-access-anywhere.svg";
+import iconAnyFile from "./icon-any-file.svg";
+import iconArrow from "./icon-arrow.svg";
+import iconCollaboration from "./icon-collaboration.svg";
+import iconEmail from "./icon-email.svg";
+import iconLocation from "./icon-location.svg";
+import iconPhone from "./icon-phone.svg";
+import iconSecurity from "./icon-security.svg";
+import illustrationIntro from "./illustration-intro.png";
+import illustrationStayProductive from "./illustration-stay-productive.png";
+import logo from "./logo.svg";
+import profileOne from "./profile-1.jpg";
+import profileTwo from "./profile-2.jpg";
+import profileThree from "./profile-3.jpg";
+
+export {
+  bgCurvyMobile,
+  bgQuotes,
+  iconAccessAnywhere,
+  iconAnyFile,
+  iconArrow,
+  iconCollaboration,
+  iconEmail,
+  iconLocation,
+  iconPhone,
+  iconSecurity,
+  illustrationIntro,
+  illustrationStayProductive,
+  logo,
+  profileOne,
+  profileTwo,
+  profileThree,
+};

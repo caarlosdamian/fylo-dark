b494147ef2fb5cd87c1f9bf6d1b5e2aab8e63fe0
caarlosdamian <dapecarlos@gmail.com>
2023-03-26 18:55:04 -0600

    feat(features): added features section

--- src/sections/features/Features.tsx
+++ src/sections/features/Features.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import { Button } from "../../components/button/Button";
+import { featuresInfo } from "../../utils";
+import { Slide } from "react-awesome-reveal";
 import "./Features.scss";
+
 export const Features = () => {
   return (
     <section className="features">
       <div className="features__wrapper">
-        <h1 className="features__wrapper--title">All your files in one secure location, accessible anywhere.</h1>
+        <h1 className="features__wrapper--title">
+          All your files in one secure location, accessible anywhere.
+        </h1>
         <span className="features__wrapper--subtitle">
           Fylo stores all your most important files in one secure location.
           Access them wherever you need, share and collaborate with friends
           family, and co-workers.
         </span>
-        {/* <button>Get Started</button> */}
-        <Button label="Get Started"/>
+        <Button label="Get Started" />
+      </div>
+      <div className="features__grid">
+        {featuresInfo.map((item) => (
+          <Slide triggerOnce>
+            <div className="feature">
+              <img src={item.img} alt={item.img} className="feature__img" />
+              <h2 className="feature__title">{item.title}</h2>
+              <span className="feature__desc">{item.desc}</span>
+            </div>
+          </Slide>
+        ))}
       </div>
     </section>
   );
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,10 @@
+import {
+  iconAccessAnywhere,
+  iconAnyFile,
+  iconCollaboration,
+  iconSecurity,
+} from "../assets";
+
 export const navLinks = [
   {
     id: 1,
@@ -10,5 +17,32 @@ export const navLinks = [
   {
     id: 3,
     title: "Sign In",
+  },
+];
+
+export const featuresInfo = [
+  {
+    id: "1a",
+    title: "Access your files, anywhere",
+    desc: "The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.",
+    img: iconAccessAnywhere,
+  },
+  {
+    id: "2a",
+    title: "Security you can trust",
+    desc: "2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files.",
+    img: iconSecurity,
+  },
+  {
+    id: "3a",
+    title: "Real-time collaboration",
+    desc: "Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.",
+    img: iconCollaboration,
+  },
+  {
+    id: "4a",
+    title: "Store any type of file",
+    desc: "Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared.",
+    img: iconAnyFile,
   },
 ];

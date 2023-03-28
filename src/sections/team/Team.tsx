15d33170a5fbc9baa3ddf2e231e17d473cfe9c9b
caarlosdamian <dapecarlos@gmail.com>
2023-03-27 19:53:28 -0600

    feat(team): added team section

--- src/sections/team/Team.tsx
+++ src/sections/team/Team.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
-import './Team.scss'
+import React from "react";
+import { iconArrow, illustrationStayProductive } from "../../assets";
+import "./Team.scss";
 
 export const Team = () => {
   return (
-    <div>Team</div>
-  )
-}
+    <section className="team">
+      <img src={illustrationStayProductive} alt="illustrationStayProductive" />
+      <div className="team__info">
+        <h2 className="team__info--title">Stay productive, wherever you are</h2>
+        <span className="team__info--desc">
+          Never let location be an issue when accessing your files. Fylo has you
+          covered for all of your file storage needs.
+        </span>
+        <span className="team__info--desc">
+          Securely share files and folders with friends, family and colleagues
+          for live collaboration. No email attachments required.
+        </span>
+        <div className="team__info--button">
+          <span className="button__label">See how Fylo works</span>
+          <img src={iconArrow} alt={iconArrow} />
+        </div>
+      </div>
+    </section>
+  );
+};

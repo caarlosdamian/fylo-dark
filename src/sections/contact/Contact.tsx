d48121096df7a1f191c65a5aea6daee9ccd649a8
caarlosdamian <dapecarlos@gmail.com>
2023-03-27 20:51:12 -0600

    feat(contact): added contact section

--- src/sections/contact/Contact.tsx
+++ src/sections/contact/Contact.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Button } from '../../components/button/Button';
 import './Contact.scss'
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className='contact'>
+      <div className="contact__wrapper">
+       <div className="contact__wrapper--info">
+       <span className="title">Get early access today</span>
+       <span className="desc">It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you.</span>
+       </div>
+       <div className="contact__wrapper--controls">
+        <input type="text" className="input" placeholder='email@example.com' />
+        <Button label='Get Started For Free' />
+
+       </div>
+      </div>
+    </section>
   )
 }
